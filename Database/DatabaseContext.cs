d3c21888219ad473b7994d2903d8a25bdd5d3547
Alex McClure <amcclure@vermeer.com>
2023-12-29 12:48:19 -0600

    Initial commit

---
+++ Database/DatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+using Npgsql;
+
+namespace Database;
+
+public class DatabaseContext(string connectionString)
+{
+    public DbConnection Connection { get; } = new NpgsqlConnection(connectionString);
+}
